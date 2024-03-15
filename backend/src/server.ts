3d843cb7f6168282edc96b57fec52357a36bcc63
ChiragWadhwani <wadhwanichirag94@gmail.com>
2024-03-15 23:48:45 +0530

    Basic server setup

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+config({ path: '.env' });
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
