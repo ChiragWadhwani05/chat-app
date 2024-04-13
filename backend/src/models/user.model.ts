cee9a97f1fa04cb8aa564a616bfd01b7bfd1cdf1
ChiragWadhwani <wadhwanichirag94@gmail.com>
2024-04-13 14:19:21 +0530

    features add for hash and comapre  password, generate refresh and access token

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,34 +1,82 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    fullname: {
-      type: String,
-    },
-    password: {
-      type: String,
-      required: true,
-      Selection: false,
-    },
-    avatar: {
-      type: String,
-      default: '',
-    },
-    email: {
-      type: String,
-    },
-    bio: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+			index: true,
+		},
+		fullname: {
+			type: String,
+		},
+		password: {
+			type: String,
+			required: true,
+			Selection: false,
+		},
+		avatar: {
+			type: String,
+			default: "",
+		},
+		email: {
+			type: String,
+			unique: true,
+			lowercase: true,
+			trim: true,
+			index: true,
+		},
+		bio: {
+			type: String,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
-export const User = mongoose.model('User', userSchema);
+// Hash password before saving into database
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) {
+		return next();
+	}
+	const salt = await bcryptjs.genSalt(10);
+	this.password = await bcryptjs.hash(this.password, salt);
+	next();
+});
+
+// Generate access token
+userSchema.methods.generateAccessToken = function () {
+	return jwt.sign(
+		{ userId: this._id, username: this.username },
+		process.env.ACCESS_TOKEN_SECRET || "chatApp",
+		{
+			expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+		}
+	);
+};
+
+// Generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+	return jwt.sign(
+		{ userId: this._id },
+		process.env.REFRESH_TOKEN_SECRET || "chatApp",
+		{
+			expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+		}
+	);
+};
+
+// Check password
+userSchema.methods.isPasswordCorrect = async function (
+	candidatePassword: string
+) {
+	return await bcryptjs.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
