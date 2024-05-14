498bd3637cd1878e15c23852709ddc97140b41ed
ChiragWadhwani <wadhwanichirag94@gmail.com>
2024-05-14 21:11:49 +0530

    Mesaage Feature create and get

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes";
 import chatRoutes from "./routes/chat.routes";
+import messageRoutes from "./routes/message.routes";
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use(cookieParser());
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/message", messageRoutes);
 
 export { app };
